a058dda0c5093c8de9ffd73eba909004bde0a570
hamagami.kanae <hamagami.kanea@dsic.jp>
2025-09-24 17:52:50 +0900

    ホーム画面の修正

--- src/app/components/home/Home.tsx
+++ src/app/components/home/Home.tsx
@@ -1,12 +1,22 @@
 import { HomeTabs } from "@/app/components/home/HomeTabs";
 
 export const Home = () => {
+  const today = new Date();
+  const formatted = today
+    .toLocaleDateString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+    })
+    .split("/")
+    .join(".");
+  console.log(today);
+  console.log(formatted);
   return (
     <>
       <div className="w-[350px]">
         <div className="flex justify-center mt-3 flex-col items-center gap-2">
-          <h1  className="text-maincolor text-xl font-bold font-mono ">
-            2025.09
+          <h1 className="text-maincolor text-xl font-bold font-mono ">
+            {formatted}
           </h1>
           <h1 className="text-2xl font-bold font-mono text-[#F06E9C]">
             ¥200,000
--- src/app/components/molecules/ButtonGroup.tsx
+++ src/app/components/molecules/ButtonGroup.tsx
@@ -1,7 +1,7 @@
 type ButtonGroupProps = {
   label: string;
-  varient?: "expense" | "income";
-  onClick?:(e: React.MouseEvent<HTMLInputElement>) => void
+  varient?: "expense" | "income" | "budget";
+  onClick?: (e: React.MouseEvent<HTMLInputElement>) => void;
 };
 
 export const ButtonGroup: React.FC<ButtonGroupProps> = ({
@@ -15,7 +15,9 @@ export const ButtonGroup: React.FC<ButtonGroupProps> = ({
         className={
           varient === "expense"
             ? "btn rounded-[3px] bg-[#A8C6FA] text-white btn-xl btn-wide shadow-lg text-lg hover:bg-[#75A9F9] w-[165px] h-[47px]"
-            : "btn rounded-[3px] bg-[#F6A2BF] text-white btn-xl btn-wide shadow-lg text-lg hover:bg-[#F06E9C] w-[165px] h-[47px]"
+            : varient === "income"
+            ? "btn rounded-[3px] bg-[#F6A2BF] text-white btn-xl btn-wide shadow-lg text-lg hover:bg-[#F06E9C] w-[165px] h-[47px]"
+            : "btn rounded-[3px] bg-[#AAAAAA] text-white shadow-lg text-sm hover:bg-[#929292] w-[131px] h-[40px]"
         }
         onClick={onClick}
       >
